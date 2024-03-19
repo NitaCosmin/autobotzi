ae0940cfc438377606e7a94cd060426f45493e96
Nita <102913196+NitaCosmin@users.noreply.github.com>
2024-03-19 17:09:07 +0200

    CORS

--- src/main/java/autobotzi/config/WebConfig.java
+++ src/main/java/autobotzi/config/WebConfig.java
@@ -12,11 +12,16 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://atc-2024-autobotzi-fe-linux-web-app.azurewebsites.net/")
-                .allowedOrigins("https://frontend-jf48yfydc-eduard-ionel-eduards-projects.vercel.app/")
-                .allowedOrigins("https://front-autobotzi-c55123365842.herokuapp.com/")
                 .allowedOrigins("http://localhost:3000")  // Add your frontend URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
-
+        registry.addMapping("/**")
+                .allowedOrigins("http://atc-2024-autobotzi-fe-linux-web-app.azurewebsites.net/")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+        registry.addMapping("/**")
+                .allowedOrigins("https://frontend-jf48yfydc-eduard-ionel-eduards-projects.vercel.app/")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+        registry.addMapping("/**")
+                .allowedOrigins("https://front-autobotzi-c55123365842.herokuapp.com/")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 }
