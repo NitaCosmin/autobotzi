a3ca039c99440bf27f222ac0469b9a29a7deda93
Nita <102913196+NitaCosmin@users.noreply.github.com>
2024-03-19 14:58:10 +0200

    web config

--- src/main/java/autobotzi/config/WebConfig.java
+++ src/main/java/autobotzi/config/WebConfig.java
@@ -14,6 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("http://atc-2024-autobotzi-fe-linux-web-app.azurewebsites.net/")
                 .allowedOrigins("https://frontend-jf48yfydc-eduard-ionel-eduards-projects.vercel.app/")
+                .allowedOrigins("https://front-autobotzi-c55123365842.herokuapp.com/")
                 .allowedOrigins("http://localhost:3000")  // Add your frontend URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
 
